5cf5916db0aef247716d34baa694fa9433c1a821
Yiecheverría <yiecheverria@uce.edu.ec>
2023-02-09 02:04:52 -0500

    Initial commit

---
+++ src/main/java/uce/edu/ec/migracion/MigracionApplication.java
@@ -0,0 +1,13 @@
+package uce.edu.ec.migracion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigracionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MigracionApplication.class, args);
+    }
+
+}
